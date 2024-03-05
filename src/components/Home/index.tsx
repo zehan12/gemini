efa9a6664b9c330be1bf4f258f726492ef0df054
Zehan Khan <zkhan@hootboard.com>
2024-03-06 01:38:14 +0530

    feat: :house: home page ui created

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,1 @@
+export { GoogleGeminiEffect } from "./GoogleGeminiEffect";
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,14 +7,40 @@ import Routes from "../constants/routes.enum";
 // routes
 import { rootRoute } from "../App";
 
-function Home() {
+import { useScroll, useTransform } from "framer-motion";
+import React from "react";
+import { GoogleGeminiEffect } from "../components/Home/GoogleGeminiEffect";
+
+export function Home() {
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
   return (
-    <div style={{ display: "flex", height: "100vh", placeContent: "center" }}>
-      <div>Home Page</div>
+    <div
+      className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
+      ref={ref}
+    >
+      <GoogleGeminiEffect
+        pathLengths={[
+          pathLengthFirst,
+          pathLengthSecond,
+          pathLengthThird,
+          pathLengthFourth,
+          pathLengthFifth,
+        ]}
+      />
     </div>
   );
 }
-
 // Index route
 export const homeRoute = new Route({
   component: Home,
