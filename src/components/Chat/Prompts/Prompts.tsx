31cbe22ecab8ea9f6a466a392e64728a97c19961
Zehan Khan <zkhan@hootboard.com>
2024-03-08 01:54:21 +0530

    feat: :fried_shrimp: added prompt component

---
+++ src/components/Chat/Prompts/Prompts.tsx
@@ -0,0 +1,38 @@
+import { CircleDollarSign } from "lucide-react";
+
+const promptData = [
+  {
+    question: "How to Implement Linked List in JavaScript, Python and Java",
+    tag: "code",
+  },
+  {
+    question: "Help me Find latest trend",
+    tag: "Trends",
+  },
+  {
+    question: "create a study plan how to complete DSA in 6 months ",
+    tag: "writing",
+  },
+  {
+    question: "Find hotels for a New Year’s trip to San Francisco",
+    tag: "travel",
+  },
+];
+
+export const Prompts = () => {
+  return (
+    <div className="container flex gap-3 justify-center">
+      {promptData.map((item) => (
+        <div
+          className=" w-56 h-56 p-4 flex flex-col justify-between rounded-xl bg-zinc-800/90 hover:bg-zinc-700 cursor-pointer"
+          key={item.question}
+        >
+          <h3 className="text-sm line-clamp-3 leading-6">{item.question}</h3>
+          <div className="flex justify-end p-1">
+            <CircleDollarSign className="w-10 h-10 rounded-full bg-black p-1.5" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
